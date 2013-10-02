ac2bcafb9336fc63b010e180ea9994acd7edc211
Alexander Daniel <alexander.daniel@gmx.at>
2013-10-02 20:40:00 +0200

    end of dojo

--- src/main/scala/dojo/Wrapper.scala
+++ src/main/scala/dojo/Wrapper.scala
@@ -3,6 +3,24 @@ package dojo
 object Wrapper {
 
   def wrap(text: String, col: Int): String = {
-    text
+    text.split("\n").map(l => wrapLine(l, col)).mkString("\n")
   }
+
+  def wrapLine(text: String, col: Int): String = {
+    if(text.size>col)
+      {
+        val first: String = text.take(col)
+        if (first.endsWith(" "))
+          first.substring(0, first.size - 2) + "\n" + text.drop(col)
+        else
+          first + "\n" + text.drop(col)
+      }
+    else
+      text
+  }
+
+  def splitIntoWords(line : String): Seq[String] = {
+    line.split(" ")
+  }
+
 }
--- src/test/scala/dojo/WrapperSuite.scala
+++ src/test/scala/dojo/WrapperSuite.scala
@@ -14,9 +14,21 @@ class WrapperSuite extends FunSuite {
   }
 
   test("text where each line is shorter than col num") {
-    assert("""a b c d.\n\ne a fe\n""" === wrap("""a b c d.\n\ne a fe\n""", 10))
+    assert("a b c d.\ne a fe" === wrap("a b c d.\ne a fe", 10))
   }
-//  test("one line text with line longer than column number"){
-//    assert("""12345\n1234""" === wrap("""12345 1234""",7))
+
+  //TODO write another test with a trailing new line
+  //TODO write another test with two new lines next to each other
+//  test("one line text swith line longer than column number"){
+//    assert("12345\n1234" === wrap("12345 1234",7))
 //  }
+
+  test("one word text longer than column width") {
+    assert("1234\n5"=== wrap("12345",4))
+  }
+
+  test("split line into words"){
+    assert(splitIntoWords("a bad character defines") === Seq("a", "bad", "character", "defines"))
+  }
+
 }
